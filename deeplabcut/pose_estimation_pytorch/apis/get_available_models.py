573d319321c1b74c35ff9f2e5869e89e3d2e6ed0
fabionaecht <fabio.naecht@univie.ac.at>
2024-10-01 08:37:53 +0200

    ADD

---
+++ deeplabcut/pose_estimation_pytorch/apis/get_available_models.py
@@ -0,0 +1,7 @@
+import deeplabcut.pose_estimation_pytorch
+
+# Available pose estimation models
+print(deeplabcut.pose_estimation_pytorch.available_models())
+
+# Available object detection models
+print(deeplabcut.pose_estimation_pytorch.available_detectors())
