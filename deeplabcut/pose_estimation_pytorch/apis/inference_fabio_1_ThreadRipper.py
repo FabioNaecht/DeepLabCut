6d7c8bf47e4271e86e57e27154ba5edd9904a47b
FabioNaecht <fabio.naecht@univie.ac.at>
2024-11-14 17:05:22 +0100

    threadripper

--- deeplabcut/pose_estimation_pytorch/apis/inference_fabio_1_ThreadRipper.py
+++ deeplabcut/pose_estimation_pytorch/apis/inference_fabio_1_ThreadRipper.py
@@ -4,17 +4,17 @@ import torch
 import cv2
 import numpy as np
 
-from deeplabcut.pose_estimation_pytorch.apis.analyze_videos import video_inference, video_inference_shrunk, inference_single_img
+from deeplabcut.pose_estimation_pytorch.apis.analyze_videos import video_inference
 from deeplabcut.pose_estimation_pytorch.config import read_config_as_dict
 from deeplabcut.pose_estimation_pytorch.task import Task
 from deeplabcut.pose_estimation_pytorch.apis.utils import get_inference_runners
 
-train_dir = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/20230424_nn200_tip-sam/dlc-models-pytorch/iteration-1/20230424_nn200_tipApr24-trainset95shuffle1/train/")
+train_dir = Path("/home/zimadmin/Documents/20230424_nn200_tip-sam/dlc-models-pytorch/iteration-1/20230424_nn200_tipApr24-trainset95shuffle1/train/")
 pytorch_config_path = train_dir / "pytorch_config.yaml"
 snapshot_path = train_dir / "snapshot-200.pt"
 
 # video and inference parameters
-video_path = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/20230424_nn200_tip-sam/test-videos/2023_04_22_15_00_tracking_trial_control_fabio_image_stack1.avi")
+video_path = Path("/home/zimadmin/Documents/20230424_nn200_tip-sam/videos/2023_04_22_15_00_tracking_trial_control_fabio_image_stack1.avi")
 max_num_animals = 1
 batch_size = 16
 
@@ -45,7 +45,7 @@ print(f"Pose runner: {pose_runner}")
 
 # import cv2
 # import numpy as np
-#
+
 # # Set video path and prediction results
 # predictions = video_inference(
 #     video_path=video_path,
@@ -54,9 +54,9 @@ print(f"Pose runner: {pose_runner}")
 #     detector_runner=detector_runner,
 #     with_identity=False,
 # )
-#
+
 # print(f"Predictions: {predictions}")
-#
+
 # # Open the video
 # cap = cv2.VideoCapture(video_path)
 #
